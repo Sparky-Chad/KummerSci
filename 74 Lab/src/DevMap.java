d00566f7a79a9c792f1af3738a8f2c5c64c7a32d
Chad Lape <cjlonghorn>
2018-11-07 13:36:32 +0000

    Finished 7.4 and pong game

---
+++ 74 Lab/src/DevMap.java
@@ -0,0 +1,8 @@
+
+public interface DevMap {
+	
+	public static final int Height = 500;
+	public static final int Width = 600;
+	public static final double toMonth = 12;
+	public static final double toYear = 1/12;
+}
--- TopTen/src/Main.java
+++ TopTen/src/Main.java
@@ -1,4 +1,6 @@
 import java.io.*;
+
+
 public class Main {
 	public static void main(String args[]) {
 		String path = "/home/cjlonghorn/workspace/TopTen/src/dat.txt";
