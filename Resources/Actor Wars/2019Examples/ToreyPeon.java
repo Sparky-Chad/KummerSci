688a0711ec789af9a0bd8ef9588d6086da61bdbb
Chad Lape <cjlonghorn@gmail.com>
2019-05-01 20:43:32 -0400

    added resources into the github project

---
+++ Resources/Actor Wars/2019Examples/ToreyPeon.java
@@ -0,0 +1,22 @@
+import org.masonacm.actorwars.LocationFinder;
+import org.masonacm.actorwars.Peon;
+import org.masonacm.actorwars.Wheat;
+
+import com.gawdl3y.util.DynamicValue;
+
+import info.gridworld.grid.Location;
+
+public class ToreyPeon extends Peon {
+
+	@Override
+	public void peonAct() {
+		if (myactions.size() == 0) {
+			DynamicValue<Location> l = LocationFinder
+					.findClosestInstanceDynamicLocation(getDynamicLocation(), Wheat.class, getGrid());
+			if (l.getValue() != null) {
+				add(Peon.moveToGradual(l));
+			}		
+		}
+	}
+
+}
